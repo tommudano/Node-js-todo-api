5bcf2978d7310f1566c5f2dec3e666393864d9cd
Tomás Mudano <mudano.tomas@gmail.com>
2019-01-17 15:33:48 -0300

    Upload to heroku v2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+mongoose.connect('mongodb://tommudano:bycicle01.@ds159574.mlab.com:59574/todoapp' || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
 
 
 module.exports = {mongoose};
