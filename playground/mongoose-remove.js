fdc1ba55280e845871898b138e2ce4f7b141744b
Tomás Mudano <mudano.tomas@gmail.com>
2019-01-21 15:38:32 -0300

    new update

--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -1,15 +1,15 @@
-const {ObjectID} = require('mongodb');
-
-const {mongoose} = require('./../server/db/mongoose');
-const {Todo} = require('./../server/models/todo');
-const {User} = require('./../server/models/user');
-
-// Todo.remove({}).then((result) => {
-//   console.log(result);
+// const {ObjectID} = require('mongodb');
+//
+// const {mongoose} = require('./../server/db/mongoose');
+// const {Todo} = require('./../server/models/todo');
+// const {User} = require('./../server/models/user');
+//
+// // Todo.remove({}).then((result) => {
+// //   console.log(result);
+// // });
+// //
+// // Todo.findOneAndRemove();
+// //
+// Todo.findOneAndDelete({_id: '5c45da1890d53325acd27260'}).then((todo) => {
+//   console.log(todo);
 // });
-//
-// Todo.findOneAndRemove();
-//
-Todo.findOneAndDelete({_id: '5c45da1890d53325acd27260'}).then((todo) => {
-  console.log(todo);
-});
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://tommudano:bycicle01.@ds159574.mlab.com:59574/todoapp' || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+mongoose.connect('mongodb://tommudano:bycicle01.@ds159574.mlab.com:59574/todoapp' || 'mongodb://127.0.0.1:27017/TodoApp', { useNewUrlParser: true });
 
 
 module.exports = {mongoose};
