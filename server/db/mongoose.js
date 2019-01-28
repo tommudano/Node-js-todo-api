9c7e08c158c1f45a5ee940f487ce0e5dfcf9f1d4
Tomás Mudano <mudano.tomas@gmail.com>
2019-01-28 19:02:31 -0300

    Update before deployment

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,6 +4,6 @@ let mongoose = require('mongoose');
 // .set('debug', true)
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://tommudano:bycicle01.@ds159574.mlab.com:59574/todoapp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
